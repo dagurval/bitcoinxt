Bitcoin-ABC/bitcoin-abc

Impose a minimum transaction size of 100 bytes after the Nov, 15 2018 HF
Summary:
This ensure we are not vulnerable to the leaf node weakness in bitcoin's merkle tree design anymore and give us room to evolve the protocol in the future (or not).

For more information about the weakness, please read https://bitslog.wordpress.com/2018/06/09/leaf-node-weakness-in-bitcoin-merkle-tree-design/ ( http://archive.is/xA9mW ).

Test Plan:
Added unit test that ensure transaction size is properly checked after activation.
Added an integration test to check for activation.

Reviewers: #bitcoin_abc, Mengerian, jasonbcox

Reviewed By: #bitcoin_abc, Mengerian, jasonbcox

Subscribers: jasonbcox, Mengerian, teamcity

Differential Revision: https://reviews.bitcoinabc.org/D1611

src/consensus/consensus.h
@@ -12,6 +12,8 @@
 static const uint64_t ONE_MEGABYTE = 1000000;
 /** The maximum allowed size for a transaction, in bytes */
 static const uint64_t MAX_TX_SIZE = ONE_MEGABYTE;
+/** The minimum allowed size for a transaction, in bytes */
+static const uint64_t MIN_TX_SIZE = 100;
 /** The maximum allowed size for a block, before the UAHF */
 static const uint64_t LEGACY_MAX_BLOCK_SIZE = ONE_MEGABYTE;
 /** Default setting for maximum allowed size for a block, in bytes */

dagurval/bitcoinxt

src/consensus/consensus.h
@@ -16,6 +16,8 @@ static const unsigned int UAHF_INITIAL_MAX_BLOCK_SIZE = 8000000;
 static const unsigned int THIRD_HF_INITIAL_MAX_BLOCK_SIZE = 32000000;
 /** The maximum allowed size for a serialized transaction, in bytes */
 static const unsigned int MAX_TRANSACTION_SIZE = 1000*1000;
+/** The minimum allowed size for a transaction, in bytes */
+static const uint64_t MIN_TRANSACTION_SIZE = 100;
 /** The maximum allowed number of signature check operations in a block (network rule) */
 inline uint64_t MaxBlockSigops(uint64_t nBlockSize) {
     return ((nBlockSize - 1) / 1000000 + 1) * 1000000 / 50;